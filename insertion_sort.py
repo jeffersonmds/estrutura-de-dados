c280aeede35d1f825786e637fa29ccf84b97f45f
jeffmorais <jeffmorais96@gmail.com>
2016-03-30 09:28:12 -0300

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -25,6 +25,5 @@ class OrdenacaoTestes(unittest.TestCase):
     def teste_lista_desordenada(self):
         self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], insertion_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
 
-
 if __name__ == '__main__':
     unittest.main()
